51af857da1548c6ca3a2be33614837ab8d7863f5
mramu_1996 <mandaloju.ramu@gmail.com>
2024-03-27 18:15:04 -0400

    implemented chat screen

--- app/src/main/java/com/example/whatsappcomposeui/components/ChatComponent.kt
+++ app/src/main/java/com/example/whatsappcomposeui/components/ChatComponent.kt
@@ -15,16 +15,23 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.whatsappcomposeui.R
+import com.example.whatsappcomposeui.data.ChatData
+import com.example.whatsappcomposeui.data.DeliveryStatus
+import com.example.whatsappcomposeui.data.MessageDetails
+import com.example.whatsappcomposeui.data.MessageType
+import com.example.whatsappcomposeui.data.UserDetails
 
 @Composable
 fun ChatComponent() {
@@ -33,36 +40,74 @@ fun ChatComponent() {
 
 
 @Composable
-fun SingleChat() {
-    Row(modifier = Modifier.fillMaxWidth().height(80.dp).padding(1.dp)) {
-        Icon(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "profile picture"
-        ,modifier = Modifier.requiredSize(80.dp).clip(CircleShape))
+fun SingleChat(chatData: ChatData) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .padding(1.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = chatData.userDetails.userDp),
+            contentDescription = "profile picture",
+            modifier = Modifier
+                .requiredSize(70.dp)
+                .clip(CircleShape)
+        )
         Column() {
-            Row(Modifier.padding(10.dp)) {
-// name and date
-                Text(text = "Jon Targeryen")
+            Row(Modifier.padding(5.dp)) {
+            // name and date
+                Text(text = chatData.userDetails.userName, fontWeight = FontWeight.Bold)
                 Spacer(modifier = Modifier.weight(1f))
-                Text(text = "27/02/2023")
+                Text(text = chatData.messageDetails.timeStamp, fontSize = 12.sp)
             }
-            Spacer(modifier = Modifier.weight(1f))
-            Row(Modifier.padding(10.dp)) {
-// last chat and unreadMsgs count
-                Text(text = "see you tomorrow")
+            Spacer(modifier = Modifier.height(2.dp))
+            Row(Modifier.padding(5.dp)) {
+            // last chat and unreadMsgs count
+                Text(text = chatData.messageDetails.message)
                 Spacer(modifier = Modifier.weight(1f))
-                Text(text = "3", style = TextStyle(fontSize = 12.sp, textAlign = TextAlign.Center, color = MaterialTheme.colorScheme.tertiary),
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .clip(CircleShape)
-                        .requiredSize(20.dp)
-                        .background(Color.Green)
-                )
+                HandleUnreadCount(chatData)
             }
         }
     }
 }
 
+@Composable
+private fun HandleUnreadCount(chatData: ChatData) {
+    if(chatData.messageDetails.unreadCount==0) return
+    Text(
+        text = chatData.messageDetails.unreadCount.toString(),
+        style = TextStyle(
+            fontSize = 12.sp,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.tertiary
+        ),
+        modifier = Modifier
+            .padding(4.dp)
+            .clip(CircleShape)
+            .requiredSize(20.dp)
+            .background(Color.Green)
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun SingleChatPreview() {
-    SingleChat()
+    SingleChat(
+        ChatData(
+            chatId = 2,
+            messageDetails = MessageDetails(
+                message = "I'm good, thanks!",
+                timeStamp = "2024-03-27 10:05 AM",
+                messageType = MessageType.MESSAGE,
+                deliveryStatus = DeliveryStatus.SEEN,
+                unreadCount = 0
+            ),
+            userDetails = UserDetails(
+                userName = "Bob",
+                userDp = R.drawable.img_1
+            )
+    )
+    )
 }
--- app/src/main/java/com/example/whatsappcomposeui/screens/ChatScreen.kt
+++ app/src/main/java/com/example/whatsappcomposeui/screens/ChatScreen.kt
@@ -1,20 +1,26 @@
 package com.example.whatsappcomposeui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.whatsappcomposeui.components.SingleChat
+import com.example.whatsappcomposeui.data.chatDataList
 
 @Composable
 fun ChatScreen() {
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Chat Screen")
+    LazyColumn(Modifier.padding(5.dp)){
+        items(chatDataList){
+            chatDataList.forEach {
+                SingleChat(chatData = it)
+                Spacer(modifier = Modifier.height(2.dp))
+            }
+        }
     }
 }
 
